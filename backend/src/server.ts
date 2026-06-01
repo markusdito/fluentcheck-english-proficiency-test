01ee8040336ccc4d5c47de16e05bedfa01c67030
Markus Dito <markusdito@gmail.com>
2026-06-01 17:04:48 +0700

    feat: configuring express server

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "FluentCheck API"
+    })
+})
+
+const server = app.listen(process.env.PORT, () => {
+    console.log("Server started on port: " + process.env.PORT);
+})
