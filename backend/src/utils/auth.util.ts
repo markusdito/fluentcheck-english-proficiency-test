e57d5175a4b3e0170d67949aed6877690f72c837
Markus Dito <markusdito@gmail.com>
2026-06-06 23:44:38 +0700

    refactor: restructuring auth logic

--- backend/src/utils/auth.util.ts
+++ backend/src/utils/auth.util.ts
@@ -1,12 +1,29 @@
 import jwt, { Secret, SignOptions } from 'jsonwebtoken'
 import { env } from "../config/env.js"
+import { prisma } from"../config/db.js"
+import bcrypt from "bcryptjs"
 
-export async function createUser() {
-    // Logic
+export async function createUser(username:string, email:string, password:string) {
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    //create user
+    return prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword
+        }, select: {
+            id: true,
+            username: true,
+            email: true,
+            createdAt: true,
+        }
+    })
 }
 
-export async function authenticateUser() {
-    //Logic
+export async function authenticateUser(password: string, dbPw: string) {
+    return bcrypt.compare(password, dbPw)
 }
 
 export function generateToken(userId: string) {
